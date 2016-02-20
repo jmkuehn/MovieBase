8ac1395bc3abd792b3bd04a36b6bf81c3c010c25
Kyle Thompson <kyle@kylekthompson.com>
2016-02-20 18:01:02 -0500

    some view updates

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
-           :scope => 'email,user_friends', :display => 'popup'
+           :scope => 'email,user_friends'
 end
