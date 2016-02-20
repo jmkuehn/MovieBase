66fd6cc09e199feb56be9a8b864223def3485714
Kyle Thompson <kyle@kylekthompson.com>
2016-02-20 13:16:22 -0500

    remove members and add assets

--- MovieBase/db/schema.rb
+++ MovieBase/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220101902) do
+ActiveRecord::Schema.define(version: 20160220181304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,15 +43,8 @@ ActiveRecord::Schema.define(version: 20160220101902) do
     t.datetime "updated_at",                     null: false
     t.text     "short_description", default: "", null: false
     t.text     "long_description",  default: "", null: false
-    t.text     "phone_number",      default: "", null: false
-    t.text     "website",           default: "", null: false
-  end
-
-  create_table "members", force: :cascade do |t|
-    t.string   "username"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "phone_number",      default: "", null: false
+    t.string   "website",           default: "", null: false
   end
 
   create_table "memberships", force: :cascade do |t|
