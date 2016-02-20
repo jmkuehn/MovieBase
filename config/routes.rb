c27b4457b30069e1143dce99fabdac8f93dde4d5
Kyle Thompson <kyle@kylekthompson.com>
2016-02-20 17:06:56 -0500

    sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-
   resources :members
   resources :pages
 
   root 'welcome#index'
+
+  match '/auth/:provider/callback' => 'sessions#create'
+  match '/signout' => 'sessions#destroy', :as => :signout
+  match '/signin' => 'sessions#new', :as => :signin
 end
