a112f4f45596273f4d9d72a41b80b5ffe4a9a599
Kyle Thompson <kyle@kylekthompson.com>
2016-02-20 23:56:10 -0500

    account for nil

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -61,32 +61,34 @@ class Movie < ActiveRecord::Base
   def self.search_by_string(search_string)
     response = HTTParty.get("http://metadata.sls1.cdops.net/SearchSuggestions/SystemId/e5ce3167-4e0b-4867-a8c3-c8f23aec5e71/DistributionChannel/20389393-b2e4-4f65-968e-75a5227e544c/SearchString/#{search_string.gsub(' ', '%20').html_safe}/IncludeProducts/True")
 
-    @movies = []
-    response["Products"].each do |product|
-      if product["GuidanceRatings"]
-        @ratings = []
-        product["GuidanceRatings"].each do |rating|
-          @ratings << rating["Name"]
+    if response["Products"]
+      @movies = []
+      response["Products"].each do |product|
+        if product["GuidanceRatings"]
+          @ratings = []
+          product["GuidanceRatings"].each do |rating|
+            @ratings << rating["Name"]
+          end
+          @ratings = @ratings.join(", ")
+        else
+          @ratings = "N/A"
         end
-        @ratings = @ratings.join(", ")
-      else
-        @ratings = "N/A"
+
+        @title = product["Name"] || "N/A"
+        @thumbnail = product["ThumbnailUrl"] || "N/A"
+        @short_description = product["ShortDescription"] || "N/A"
+
+        @movie = {
+          "id" => product["Id"],
+          "title" => @title,
+          "thumbnail" => @thumbnail,
+          "short_description" => @short_description,
+          "rating" => @ratings
+        }
+        @movies << @movie
       end
-
-      @title = product["Name"] || "N/A"
-      @thumbnail = product["ThumbnailUrl"] || "N/A"
-      @short_description = product["ShortDescription"] || "N/A"
-
-      @movie = {
-        "id" => product["Id"],
-        "title" => @title,
-        "thumbnail" => @thumbnail,
-        "short_description" => @short_description,
-        "rating" => @ratings
-      }
-      @movies << @movie
+    else
+      @movies = nil
     end
-
-    @movies
   end
 end
