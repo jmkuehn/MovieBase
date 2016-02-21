f8f2c071d1a79cc8a3f2b3110dc36bf49f9d6bce
Kyle Thompson <kyle@kylekthompson.com>
2016-02-21 04:27:40 -0500

    friends stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def friends
+    @user = User.find(params[:id])
+    @friends = @user.friends
+  end
 end
