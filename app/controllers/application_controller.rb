87cc9b97683c10f5a5d201e1479a74971da436e0
Kyle Thompson <kyle@kylekthompson.com>
2016-02-21 04:41:19 -0500

    sorta auth for controllers and flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  helper_method :require_authentication
 
   def current_user
     @current_user ||= session[:user_id] && User.find_by_id(session[:user_id])
+  end
+
+  def require_authentication
+    unless current_user
+      flash[:error] = "You must be logged in!"
+      redirect_to root_url
+    end
   end
 end
