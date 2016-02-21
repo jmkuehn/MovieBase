b37d5c25a151e83f75d73d537e3a81ca970a784f
Kyle Thompson <kyle@kylekthompson.com>
2016-02-20 21:05:40 -0500

    get and search for movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,16 +6,31 @@ class Movie < ActiveRecord::Base
 
   def self.get_by_id(movie_id)
     response = HTTParty.get("http://metadata.sls1.cdops.net/Product/SystemId/e5ce3167-4e0b-4867-a8c3-c8f23aec5e71/DistributionChannel/20389393-b2e4-4f65-968e-75a5227e544c/Id/#{movie_id}")
-    @movie = {
-      "id": movie_id,
-      "title": response["Product"]["Name"],
-      "thumbnail": response["Product"]["ThumbnailUrl"],
-      "short_description": response["Product"]["ShortDescription"],
-      "rating": response["Product"]["GuidanceRatings"]["Name"],
 
+    @movie = {
+      "id" => movie_id,
+      "title" => response["Product"]["Name"],
+      "thumbnail" => response["Product"]["ThumbnailUrl"],
+      "short_description" => response["Product"]["ShortDescription"],
+      "rating" => response["Product"]["GuidanceRatings"][0]["Name"]
     }
   end
 
   def self.search_by_string(search_string)
+    response = HTTParty.get("http://metadata.sls1.cdops.net/SearchSuggestions/SystemId/e5ce3167-4e0b-4867-a8c3-c8f23aec5e71/DistributionChannel/20389393-b2e4-4f65-968e-75a5227e544c/SearchString/#{search_string.gsub(' ', '%20').html_safe}/IncludeProducts/True")
+
+    @movies = []
+    response["Products"].each do |product|
+      @movie = {
+        "id" => product["Id"],
+        "title" => product["Name"],
+        "thumbnail" => product["ThumbnailUrl"],
+        "short_description" => product["ShortDescription"],
+        "rating" => product["GuidanceRatings"][0]["Name"]
+      }
+      @movies << @movie
+    end
+
+    @movies
   end
 end
